4c3607997e730b8a0b1cf42554b7239b2ccb0101
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-04-25 12:43:10 +0200

    Added SSL checkbox and clearTransfers works now

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,29 +1,56 @@
 import React from 'react';
 import { ButtonToolbar, Button } from 'reactstrap';
+import { Checkbox } from 'react-bootstrap';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { addTransfer, clearTransfers } from '../actions/UserActions';
+import { addTransfer, compileTransfers, clearTransfers, toggleSSL } from '../actions/UserActions';
 
-const ActionButtons = ({ addTransfer, compileTransfers, clearTransfers }) => (
+const ActionButtons = ({
+  addTransfer, compileTransfers, clearTransfers, toggleSSL, ssl,
+}) => (
   <ButtonToolbar>
     <Button color="info" onClick={() => addTransfer(1)}>Add Transfer</Button>
-    <Button color="warning" className="pull-right" onClick={clearTransfers}>Clear</Button>
+    <Button
+      color="warning"
+      className="pull-right"
+      onClick={() => {
+        clearTransfers();
+        compileTransfers();
+      }}
+    >Clear
+    </Button>
+    <Checkbox
+      className="pull-right"
+      inline
+      onChange={() => {
+        toggleSSL();
+        compileTransfers();
+      }}
+      checked={ssl}
+    >Toggle SSL
+    </Checkbox>
   </ButtonToolbar>
 );
 
 ActionButtons.propTypes = {
   addTransfer: PropTypes.func.isRequired,
+  compileTransfers: PropTypes.func.isRequired,
+  clearTransfers: PropTypes.func.isRequired,
+  toggleSSL: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   transfers: state.transfer.transfers,
+  ssl: state.compiler.ssl,
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
   addTransfer,
+  compileTransfers,
   clearTransfers,
+  toggleSSL,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(ActionButtons);
