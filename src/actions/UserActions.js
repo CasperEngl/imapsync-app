d1601736b9937917c3593f4e2e046cd1a3c88684
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-04-26 08:24:55 +0200

    added new SSL args and checks if passwords are default

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -3,7 +3,8 @@ eslint
 
 no-restricted-syntax: 0,
 no-multi-spaces: 0,
-camelcase: 0
+camelcase: 0,
+quotes: 0
 */
 
 import { isEmail, isURL, isEmpty } from 'validator';
@@ -34,15 +35,17 @@ export function compileTransfers() {
       const host_2      = input.host_2      || 'HOST_2';
       const user_2      = input.user_2      || 'USER_2';
       const password_2  = input.password_2  || 'PASSWORD_2';
-      const ssl_1       = state.compiler.ssl ? ' --ssl1' : '';
-      const ssl_2       = state.compiler.ssl ? ' --ssl2' : '';
+      const ssl_1       = state.compiler.ssl ? ` --ssl1 --sslargs1 'SSL_cipher_list=DEFAULT'` : '';
+      const ssl_2       = state.compiler.ssl ? ` --ssl2 --sslargs2 'SSL_cipher_list=DEFAULT'` : '';
 
       if (isURL(host_1) &&
           isURL(host_2) &&
           isEmail(user_1) &&
           isEmail(user_2) &&
-          isEmpty(password_1) !== true &&
-          !isEmpty(password_2) !== true) {
+          password_1 !== 'PASSWORD_1' &&
+          !isEmpty(password_1) &&
+          password_2 !== 'PASSWORD_2' &&
+          !isEmpty(password_2)) {
         command += `/Applications/imapsync/imapsync_bin_Darwin --host1 ${host_1} --user1 ${user_1} --password1 ${password_1}${ssl_1} --host2 ${host_2} --user2 ${user_2} --password2 ${password_2}${ssl_2}; `;
       }
     }
