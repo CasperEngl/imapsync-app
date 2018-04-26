d042051693d0edef28fb409ecc592f2c8e648f5c
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-04-26 10:42:36 +0200

    Removed Bootstrap 3 col-xs fix because it was moved to css

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -25,13 +25,13 @@ class Transfer extends PureComponent {
 
     return (
       <Fragment>
-        <Col xs="12" md="6" className={`transfer-${number} col-xs-12`}>
+        <Col xs="12" md="6" className={`transfer-${number}`}>
           <h2>Transfer {number}</h2>
           <Row>
-            <Col xs="12" md="6" className="col-xs-12">
+            <Col xs="12" md="6">
               <UserInputs number={number} user={1} />
             </Col>
-            <Col xs="12" md="6" className="col-xs-12">
+            <Col xs="12" md="6">
               <UserInputs number={number} user={2} />
             </Col>
           </Row>
