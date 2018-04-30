05df54f2f013c71183f856317b809f8f5f74a0c1
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-04-30 08:11:38 +0200

    Added duplicate transfer case in reducer and set id for initialState

--- src/reducers/transfers.js
+++ src/reducers/transfers.js
@@ -5,7 +5,7 @@ no-plusplus: 0,
 no-case-declarations: 0
 */
 
-import { ADD_TRANSFER, REMOVE_TRANSFER, UPDATE_TRANSFER_DATA, CLEAR_TRANSFERS } from '../actions/UserActions';
+import { ADD_TRANSFER, REMOVE_TRANSFER, UPDATE_TRANSFER_DATA, CLEAR_TRANSFERS, DUPLICATE_TRANSFER } from '../actions/UserActions';
 
 const initialState = {
   transfers: [
@@ -17,6 +17,7 @@ const initialState = {
   inputs: [
     {},
     {
+      id: 1,
       host_1: '',
       user_1: '',
       password_1: '',
@@ -46,6 +47,25 @@ export default function (state = initialState, action) {
           host_2: initialState.inputs[1].host_2,
           user_2: initialState.inputs[1].user_2,
           password_2: initialState.inputs[1].password_2,
+        }],
+      };
+    case DUPLICATE_TRANSFER:
+      const duplicate = state.inputs.filter(input => input.id === data)[0];
+
+      return {
+        ...state,
+        transfersCount: state.transfersCount + 1,
+        transfers: [...state.transfers, {
+          id: state.transfersCount,
+        }],
+        inputs: [...state.inputs, {
+          id: state.transfersCount,
+          host_1: duplicate.host_1,
+          user_1: duplicate.user_1,
+          password_1: duplicate.password_1,
+          host_2: duplicate.host_2,
+          user_2: duplicate.user_2,
+          password_2: duplicate.password_2,
         }],
       };
     case REMOVE_TRANSFER:
