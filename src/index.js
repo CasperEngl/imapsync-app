df43133537745f10bade2bab8fc47c07457fa24e
Web-Koncept <web-koncept@WKT-Caspers-iMac.local>
2018-05-01 07:59:37 +0200

    Moved bootstrap, jquery and font-awesome from index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+/*
+eslint
+
+import/extensions: 0,
+import/first: 0,
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/darkly/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
