338a38d9e6f48b4658230f62e5f3880f6425dbb4
Casper Engelmann <cme@web-koncept.dk>
2019-01-04 13:25:34 +0100

    Added menu and temporary preferences

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,17 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  ipcMain,
+} = require('electron');
+const ElectronPreferences = require('electron-preferences');
 const notifier = require('node-notifier');
 const appRootDir = require('app-root-dir');
 const path = require('path');
 const isDev = require('electron-is-dev');
 const shell = require('shelljs');
 const { spawn } = require('child_process');
+const os = require('os');
 
 const getPlatform = require('./get-platform');
 
@@ -15,6 +22,194 @@ const execPath = isDev
 shell.config.execPath = shell.which('node');
 
 let mainWindow;
+
+const preferences = new ElectronPreferences({
+  dataStore: path.resolve(app.getPath('userData'), 'preferences.json'),
+  defaults: {
+    notes: {
+      folder: path.resolve(os.homedir(), 'Notes'),
+    },
+    markdown: {
+      auto_format_links: true,
+      show_gutter: false,
+    },
+    preview: {
+      show: true,
+    },
+    drawer: {
+      show: true,
+    },
+  },
+  /**
+   * If the `onLoad` method is specified, this function will be called immediately after
+   * preferences are loaded for the first time. The return value of this method will be stored as the
+   * preferences object.
+   */
+  onLoad: preferences => preferences, // eslint-disable-line
+  /**
+   * The preferences window is divided into sections. Each section has a label, an icon, and one or
+   * more fields associated with it. Each section should also be given a unique ID.
+   */
+  sections: [
+    {
+      id: 'about',
+      label: 'About You',
+      /**
+           * See the list of available icons below.
+           */
+      icon: 'single-01',
+      form: {
+        groups: [
+          {
+            /**
+                       * Group heading is optional.
+                       */
+            label: 'About You',
+            fields: [
+              {
+                label: 'First Name',
+                key: 'first_name',
+                type: 'text',
+                /**
+                               * Optional text to be displayed beneath the field.
+                               */
+                help: 'What is your first name?',
+              },
+              {
+                label: 'Last Name',
+                key: 'last_name',
+                type: 'text',
+                help: 'What is your last name?',
+              },
+              {
+                label: 'Gender',
+                key: 'gender',
+                type: 'dropdown',
+                options: [
+                  { label: 'Male', value: 'male' },
+                  { label: 'Female', value: 'female' },
+                  { label: 'Unspecified', value: 'unspecified' },
+                ],
+                help: 'What is your gender?',
+              },
+              {
+                label: 'Which of the following foods do you like?',
+                key: 'foods',
+                type: 'checkbox',
+                options: [
+                  { label: 'Ice Cream', value: 'ice_cream' },
+                  { label: 'Carrots', value: 'carrots' },
+                  { label: 'Cake', value: 'cake' },
+                  { label: 'Spinach', value: 'spinach' },
+                ],
+                help: 'Select one or more foods that you like.',
+              },
+              {
+                label: 'Coolness',
+                key: 'coolness',
+                type: 'slider',
+                min: 0,
+                max: 9001,
+              },
+              {
+                label: 'Eye Color',
+                key: 'eye_color',
+                type: 'color',
+                format: 'hex', // can be hex, hsl or rgb
+                help: 'Your eye color',
+              },
+              {
+                label: 'Hair Color',
+                key: 'hair_color',
+                type: 'color',
+                format: 'rgb',
+                help: 'Your hair color',
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      id: 'notes',
+      label: 'Notes',
+      icon: 'folder-15',
+      form: {
+        groups: [
+          {
+            label: 'Stuff',
+            fields: [
+              {
+                label: 'Read notes from folder',
+                key: 'folder',
+                type: 'directory',
+                help: 'The location where your notes will be stored.',
+              },
+              {
+                heading: 'Important Message',
+                content: '<p>The quick brown fox jumps over the long white fence. The quick brown fox jumps over the long white fence. The quick brown fox jumps over the long white fence. The quick brown fox jumps over the long white fence.</p>',
+                type: 'message',
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      id: 'space',
+      label: 'Other Settings',
+      icon: 'spaceship',
+      form: {
+        groups: [
+          {
+            label: 'Other Settings',
+            fields: [
+              {
+                label: 'Phone Number',
+                key: 'phone_number',
+                type: 'text',
+                help: 'What is your phone number?',
+              },
+              {
+                label: 'Foo or Bar?',
+                key: 'foobar',
+                type: 'radio',
+                options: [
+                  { label: 'Foo', value: 'foo' },
+                  { label: 'Bar', value: 'bar' },
+                  { label: 'FooBar', value: 'foobar' },
+                ],
+                help: 'Foo? Bar?',
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+});
+
+const menu = Menu.buildFromTemplate([
+  {
+    label: 'Menu',
+    submenu: [
+      {
+        label: 'Settings',
+        click() {
+          preferences.show();
+        },
+        accelerator: 'CmdOrCtrl+E',
+      },
+      {
+        label: 'Quit',
+        click() {
+          app.quit();
+        },
+        accelerator: 'CmdOrCtrl+Q',
+      },
+    ],
+  },
+]);
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -28,7 +223,9 @@ function createWindow() {
     },
   });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
-	mainWindow.on('closed', () => (mainWindow = null)); // eslint-disable-line
+  mainWindow.on('closed', () => (mainWindow = null)); // eslint-disable-line
+
+  Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
