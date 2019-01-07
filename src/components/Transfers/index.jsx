9c8578ca3f14e3b0eae672fe334c17a39c5a594a
Casper Engelmann <cme@web-koncept.dk>
2019-01-07 10:28:17 +0100

    added react-spring Transition

--- src/components/Transfers/index.jsx
+++ src/components/Transfers/index.jsx
@@ -7,55 +7,54 @@ no-return-assign: 0,
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Row, Col } from 'reactstrap';
-import styled from 'styled-components';
+import { Transition, animated, config } from 'react-spring';
 
 import Transfer from '../Transfer';
-
-const StyledCol = styled(Col)`
-  margin: 1.5rem 0 0;
-  padding: 0 15px;
-  flex: 0 0 100%;
-  width: 100%;
-
-  @media (min-width: 576px) {
-    flex: 0 0 calc(100% / 2);
-    width: calc(100% / 2);
-  }
-
-  @media (min-width: 768px) {
-    
-  }
-
-  @media (min-width: 992px) {
-
-  }
-`;
 
 class Transfers extends PureComponent {
   static propTypes = {
     transfers: PropTypes.arrayOf(PropTypes.object).isRequired,
   }
 
-  constructor(props) {
-    super(props);
-
-    this.transferRef = React.createRef();
-  }
-
   render() {
     const { transfers } = this.props;
 
-    return (
-      <Row>
+    const transition = {
+      from: {
+        opacity: 0,
+        height: 0,
+      },
+      enter: {
+        opacity: 1,
+        height: 'auto',
+      },
+      leave: [
         {
-          transfers.map(transfer => (
-            <StyledCol key={transfer.id}>
-              <Transfer number={transfer.id} />
-            </StyledCol>
-          ))
-        }
-      </Row>
+          opacity: 0.5,
+        },
+        {
+          opacity: 0,
+        },
+        {
+          height: 0,
+        },
+      ],
+    };
+
+    return (
+      <Transition
+        items={transfers}
+        keys={item => item.id}
+        from={transition.from}
+        enter={transition.enter}
+        leave={transition.leave}
+      >
+        {item => styles => (
+          <animated.div style={styles} config={config.default} className="w-100 my-3">
+            <Transfer number={item.id} />
+          </animated.div>
+        )}
+      </Transition>
     );
   }
 }
