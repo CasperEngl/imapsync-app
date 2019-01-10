9584cfbf394dfc0857efca4b77dbdf1f80bb73f3
Casper Engelmann <cme@web-koncept.dk>
2019-01-10 10:41:09 +0100

    refactored some functions to return promises

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -44,38 +44,41 @@ export function addTransfer() {
 }
 
 export function duplicateTransfer(number) {
-  return {
-    type: DUPLICATE_TRANSFER,
-    data: number,
+  return async function (dispatch) {
+    return dispatch({
+      type: DUPLICATE_TRANSFER,
+      data: number,
+    });
   };
 }
 
 export function compileTransfers() {
-  const state = store.getState();
-  let text = '';
-  const json = [];
+  return async function (dispatch) {
+    const state = store.getState();
+    let text = '';
+    const json = [];
 
-  for (const input of state.transfer.inputs) {
-    if (input) {
-      const {
-        host_1 = 'HOST_1',
-        user_1 = 'USER_1',
-        password_1 = 'PASSWORD_1',
-        host_2 = 'HOST_2',
-        user_2 = 'USER_2',
-        password_2 = 'PASSWORD_2',
-      } = input;
+    for (const input of state.transfer.inputs) {
+      if (input) {
+        const {
+          host_1 = 'HOST_1',
+          user_1 = 'USER_1',
+          password_1 = 'PASSWORD_1',
+          host_2 = 'HOST_2',
+          user_2 = 'USER_2',
+          password_2 = 'PASSWORD_2',
+        } = input;
 
-      const args_1      = input.args_1              ? ` ${input.args_1}` : '';
-      const args_2      = input.args_2              ? ` ${input.args_2}` : '';
-      const extraArgs   = state.compiler.extraArgs  ? ` ${state.compiler.extraArgs}` : '';
-      const ssl_1       = state.compiler.ssl        ? ` --ssl1 --sslargs1 'SSL_cipher_list=DEFAULT'` : '';
-      const ssl_2       = state.compiler.ssl        ? ` --ssl2 --sslargs2 'SSL_cipher_list=DEFAULT'` : '';
+        const args_1      = input.args_1              ? ` ${input.args_1}` : '';
+        const args_2      = input.args_2              ? ` ${input.args_2}` : '';
+        const extraArgs   = state.compiler.extraArgs  ? ` ${state.compiler.extraArgs}` : '';
+        const ssl_1       = state.compiler.ssl        ? ` --ssl1 --sslargs1 'SSL_cipher_list=DEFAULT'` : '';
+        const ssl_2       = state.compiler.ssl        ? ` --ssl2 --sslargs2 'SSL_cipher_list=DEFAULT'` : '';
 
-      const allExtraArgs_1 = `${args_1}${ssl_1}${extraArgs}`;
-      const allExtraArgs_2 = `${args_2}${ssl_2}${extraArgs}`;
+        const allExtraArgs_1 = `${args_1}${ssl_1}${extraArgs}`;
+        const allExtraArgs_2 = `${args_2}${ssl_2}${extraArgs}`;
 
-      if ((isURL(host_1) || isIP(host_1))
+        if ((isURL(host_1) || isIP(host_1))
           && (isURL(host_2) || isIP(host_2))
           && isEmail(user_1)
           && isEmail(user_2)
@@ -83,45 +86,48 @@ export function compileTransfers() {
           && !isEmpty(password_1)
           && password_2 !== 'PASSWORD_2'
           && !isEmpty(password_2)) {
-        const newCommand = `
+          const newCommand = `
             ./imapsync_bin 
             --host1 '${host_1}' --user1 '${user_1}' --password1 '${password_1}'${allExtraArgs_1} 
             --host2 '${host_2}' --user2 '${user_2}' --password2 '${password_2}'${allExtraArgs_2}; 
           `.trimLiteral();
 
-        const commands = newCommand
-          .split(' ')
-          .filter((arg) => {
-            if (!arg || arg.includes('./imapsync_bin')) {
-              return false;
-            }
+          const commands = newCommand
+            .split(' ')
+            .filter((arg) => {
+              if (!arg || arg.includes('./imapsync_bin')) {
+                return false;
+              }
 
-            return arg;
-          })
-          .map(arg => arg
-            .replace(/;$/, '')
-            .replace(/^'(.*)'$/, '$1'),
-          );
+              return arg;
+            })
+            .map(arg => arg
+              .replace(/;$/, '')
+              .replace(/^'(.*)'$/, '$1'),
+            );
 
-        json.push(commands);
-        text += newCommand;
+          json.push(commands);
+          text += newCommand;
+        }
       }
     }
-  }
 
-  return {
-    type: COMPILE_TRANSFERS,
-    data: {
-      text,
-      json,
-    },
+    return dispatch({
+      type: COMPILE_TRANSFERS,
+      data: {
+        text,
+        json,
+      },
+    });
   };
 }
 
 export function removeTransfer(number) {
-  return {
-    type: REMOVE_TRANSFER,
-    data: number,
+  return async function (dispatch) {
+    return dispatch({
+      type: REMOVE_TRANSFER,
+      data: number,
+    });
   };
 }
 
