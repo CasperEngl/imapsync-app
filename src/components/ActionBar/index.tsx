cc04edf11d84acfe4a2eecac7d3a50a728210cc6
Casper Engelmann <me@casperengelmann.com>
2019-01-27 17:30:49 +0100

    removed extra args applied to all transfers for simplicity

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
-import { ButtonToolbar, ButtonGroup, Button, Input } from 'reactstrap';
+import { ButtonToolbar, ButtonGroup, Button, Input, Label, FormGroup } from 'reactstrap';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
 
-import { StyledFormGroup, StyledLabel } from '../Styled';
-
 import { addTransfer, clearTransfers } from '../../actions/transfer';
-import {
-  compileTransfers,
-  toggleSSL,
-  addExtraArgs,
-} from '../../actions/compiler';
+import { compileTransfers, toggleSSL } from '../../actions/compiler';
 
 interface Compiler {
   ssl: boolean;
@@ -30,68 +24,46 @@ interface State {
   compiler: Compiler;
 }
 
-class ActionBar extends React.Component<Props, State> {
-  handleExtraArgs(event: { target: HTMLInputElement }) {
-    const { addExtraArgs, compileTransfers } = this.props;
-    const { value } = event.target;
-
-    addExtraArgs(value);
-    compileTransfers();
-  }
-
-  render() {
-    const {
-      addTransfer,
-      compileTransfers,
-      clearTransfers,
-      toggleSSL,
-      ssl,
-    } = this.props;
-
-    return (
-      <React.Fragment>
-        <ButtonToolbar className="actionbar--mobile-fixed">
-          <ButtonGroup>
-            <Button color="primary" onClick={addTransfer}>
-              Add Transfer
-            </Button>
-            <Button
-              color="warning"
-              onClick={() => {
-                clearTransfers();
-                compileTransfers();
-              }}
-            >
-              Clear
-            </Button>
-          </ButtonGroup>
-        </ButtonToolbar>
-        <StyledFormGroup check>
-          <StyledLabel>
-            <Input
-              type="checkbox"
-              onChange={() => {
-                toggleSSL();
-                compileTransfers();
-              }}
-              checked={ssl}
-            />
-            Toggle SSL
-          </StyledLabel>
-        </StyledFormGroup>
-        <StyledFormGroup>
-          <StyledLabel for="extra-args">
-            Extra arguments <small>(for all transfers)</small>
-          </StyledLabel>
+function ActionBar({
+  addTransfer,
+  compileTransfers,
+  clearTransfers,
+  toggleSSL,
+  ssl,
+}: Props) {
+  return (
+    <React.Fragment>
+      <ButtonToolbar className="actionbar--mobile-fixed">
+        <ButtonGroup>
+          <Button color="primary" onClick={addTransfer}>
+            Add Transfer
+          </Button>
+          <Button
+            color="warning"
+            onClick={() => {
+              clearTransfers();
+              compileTransfers();
+            }}
+          >
+            Clear
+          </Button>
+        </ButtonGroup>
+      </ButtonToolbar>
+      <FormGroup check className="mt-3">
+        <Label>
           <Input
-            id="extra-args"
-            type="text"
-            onChange={(event: any) => this.handleExtraArgs(event)}
+            type="checkbox"
+            onChange={() => {
+              toggleSSL();
+              compileTransfers();
+            }}
+            checked={ssl}
           />
-        </StyledFormGroup>
-      </React.Fragment>
-    );
-  }
+          Toggle SSL
+        </Label>
+      </FormGroup>
+    </React.Fragment>
+  );
 }
 
 const mapStateToProps = (state: State) => ({
@@ -106,7 +78,6 @@ const mapDispatchToProps = (dispatch: Dispatch) =>
       compileTransfers,
       clearTransfers,
       toggleSSL,
-      addExtraArgs,
     },
     dispatch,
   );
