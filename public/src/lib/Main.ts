213a68887b268925b645515a0b260ba0c2874e55
Casper Engelmann <me@casperengelmann.com>
2019-01-27 17:29:53 +0100

    formatting

--- public/src/lib/Main.ts
+++ public/src/lib/Main.ts
@@ -8,101 +8,111 @@ import { Command, Transfer } from './Transfer';
 import { menu } from './menu';
 
 const loadURL = serve({
-	directory: 'build',
+  directory: 'build',
 });
 
 // 7z7R1!77q
 
 export class Main {
-	public appName = 'Imapsync';
-	public appIcon = path.join(__dirname, '../../assets/icon.png');
-	public BrowserWindow: typeof BrowserWindow;
-	public powerSaveBlock: number;
+  public appName = 'Imapsync';
+  public appIcon = path.join(__dirname, '../../assets/icon.png');
+  public BrowserWindow: typeof BrowserWindow;
+  public powerSaveBlock: number;
 
-	private MainWindow!: Electron.BrowserWindow | null;
-	private Application: Electron.App;
-	private ipcMain: Electron.IpcMain;
+  private MainWindow!: Electron.BrowserWindow | null;
+  private Application: Electron.App;
+  private ipcMain: Electron.IpcMain;
 
-	constructor(app: Electron.App, browserWindow: typeof BrowserWindow, ipcMain: Electron.IpcMain) {
-		this.BrowserWindow = browserWindow;
-		this.Application = app;
-		this.ipcMain = ipcMain;
-		this.powerSaveBlock = powerSaveBlocker.start('prevent-display-sleep');
+  constructor(
+    app: Electron.App,
+    browserWindow: typeof BrowserWindow,
+    ipcMain: Electron.IpcMain,
+  ) {
+    this.BrowserWindow = browserWindow;
+    this.Application = app;
+    this.ipcMain = ipcMain;
+    this.powerSaveBlock = powerSaveBlocker.start('prevent-display-sleep');
 
-		this.onWindowAllClosed = this.onWindowAllClosed.bind(this);
-		this.onClose = this.onClose.bind(this);
-		this.onReady = this.onReady.bind(this);
-		this.onReadyToShow = this.onReadyToShow.bind(this);
-		this.initialize = this.initialize.bind(this);
-	}
+    this.onWindowAllClosed = this.onWindowAllClosed.bind(this);
+    this.onClose = this.onClose.bind(this);
+    this.onReady = this.onReady.bind(this);
+    this.onReadyToShow = this.onReadyToShow.bind(this);
+    this.initialize = this.initialize.bind(this);
+  }
 
-	public initialize() {
-		this.Application.on('window-all-closed', this.onWindowAllClosed);
-		this.Application.on('ready', this.onReady);
+  public initialize() {
+    this.Application.on('window-all-closed', this.onWindowAllClosed);
+    this.Application.on('ready', this.onReady);
 
-		this.ipcMain.on('command', async (event: Electron.Event, commands: Command[]) => {
-			const transfer = new Transfer({
-				event,
-				commands,
-				command: commands[0],
-				index: 0,
-			});
+    this.ipcMain.on(
+      'command',
+      async (event: Electron.Event, commands: Command[]) => {
+        const transfer = new Transfer({
+          event,
+          commands,
+          command: commands[0],
+          index: 0,
+        });
 
-			transfer.start();
-		});
-		this.ipcMain.on('command-cancelled', (event: Electron.Event, pid: number) => {
-			process.kill(pid);
+        transfer.start();
+      },
+    );
+    this.ipcMain.on(
+      'command-cancelled',
+      (event: Electron.Event, pid: number) => {
+        process.kill(pid);
 
-			event.sender.send('command-cancelled', pid);
-		});
+        event.sender.send('command-cancelled', pid);
+      },
+    );
 
-		Menu.setApplicationMenu(menu);
-	}
+    Menu.setApplicationMenu(menu);
+  }
 
-	private onWindowAllClosed() {
-		// this.powerSaveBlock.stop();
-		this.Application.quit();
-	}
+  private onWindowAllClosed() {
+    // this.powerSaveBlock.stop();
+    this.Application.quit();
+  }
 
-	private onClose() {
-		this.MainWindow = null;
-	}
+  private onClose() {
+    this.MainWindow = null;
+  }
 
-	private onReadyToShow() {
-		if (this.MainWindow) {
-			this.MainWindow.show();
-		}
-	}
+  private onReadyToShow() {
+    if (this.MainWindow) {
+      this.MainWindow.show();
+    }
+  }
 
-	private onReady() {
-		this.MainWindow = new BrowserWindow({
-			titleBarStyle: 'hiddenInset',
-			width: 600,
-			height: 645,
-			minWidth: 320,
-			minHeight: 645,
-			backgroundColor: '#007bff',
-			show: false,
-			webPreferences: {
-				nodeIntegration: true,
-			},
-		});
+  private onReady() {
+    this.MainWindow = new BrowserWindow({
+      titleBarStyle: 'hiddenInset',
+      width: 600,
+      height: 645,
+      minWidth: 320,
+      minHeight: 645,
+      backgroundColor: '#007bff',
+      show: false,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    });
 
-		if (this.MainWindow) {
-			if (isDev) {
-				this.MainWindow.loadURL('http://localhost:3000');
-			} else {
-				loadURL(this.MainWindow);
-			}
+    if (this.MainWindow) {
+      if (isDev) {
+        this.MainWindow.loadURL('http://localhost:3000');
+      } else {
+        loadURL(this.MainWindow);
+      }
 
-			this.MainWindow.on('closed', this.onClose);
-			this.MainWindow.once('ready-to-show', this.onReadyToShow);
-		}
+      this.MainWindow.on('closed', this.onClose);
+      this.MainWindow.once('ready-to-show', this.onReadyToShow);
+    }
 
-		notify({
-			title: this.appName,
-			icon: this.appIcon,
-			message: 'Initialized',
-		});
-	}
+    notify({
+      title: this.appName,
+      icon: this.appIcon,
+      message: 'Initialized',
+    });
+  }
 }
