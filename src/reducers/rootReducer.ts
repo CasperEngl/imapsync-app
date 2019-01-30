b62946f205a80e20130a107bcfa979437ab22d6b
Casper Engelmann <cme@web-koncept.dk>
2019-01-30 13:37:32 +0100

    added settings

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import transfer from './transfer';
 import compiler from './compiler';
+import settings from './settings';
 
 const rootReducer = combineReducers<any>({
 	transfer,
 	compiler,
+	settings,
 });
 
 export default rootReducer;
---
+++ src/reducers/settings.ts
@@ -0,0 +1,31 @@
+import { SET_OUTPUT_BACKGROUND, SET_OUTPUT_COLOR } from "../actions/settings";
+
+interface State {
+	outputBackground: string;
+	outputColor: string;
+}
+
+const initialState: State = {
+  outputBackground: '#343a40',
+  outputColor: 'rgba(255, 255, 255, 0.75)',
+};
+
+export default function(state = initialState, action: any) {
+	const { type, payload } = action;
+
+	switch (type) {
+		case SET_OUTPUT_BACKGROUND:
+			return {
+				...state,
+				outputBackground: payload.color,
+      };
+    case SET_OUTPUT_COLOR:
+      return {
+        ...state,
+        outputColor: payload.color,
+      };
+
+		default:
+			return state;
+	}
+}
