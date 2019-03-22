94aac698f65c30a5a649479f2f97e27b878660d6
Casper Engelmann <cme@web-koncept.dk>
2019-03-22 14:25:26 +0100

    step back to 0.7.0

--- public/src/electron.ts
+++ public/src/electron.ts
@@ -2,6 +2,8 @@
 import { app as application, BrowserWindow, ipcMain } from 'electron';
 import { Main } from './lib/Main';
 
+// require('update-electron-app')();
+
 const app = new Main(application, BrowserWindow, ipcMain);
 
 app.initialize();
