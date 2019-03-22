102faa179fbe560e4aef5b94af13fd2de73eb911
Casper Engelmann <cme@web-koncept.dk>
2019-03-22 10:15:32 +0100

    added export/import functionality

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -1,102 +1,159 @@
 import * as React from 'react';
-import { ButtonToolbar, ButtonGroup, Button, Input, Label, FormGroup } from 'reactstrap';
+import { ButtonToolbar, ButtonGroup, Button, Input, Label, FormGroup, Row, Col } from 'reactstrap';
 import { bindActionCreators, Dispatch } from 'redux';
 import { connect } from 'react-redux';
+import * as base64 from 'base-64';
+import * as isJSON from 'is-valid-json';
 
-import { addTransfer, clearTransfers } from '../../actions/transfer';
+import { TransferState } from '../../reducers/transfer';
+
+import { addTransfer, clearTransfers, importTransfers } from '../../actions/transfer';
 import { compileTransfers, toggleSSL } from '../../actions/compiler';
 
 interface Compiler {
-  ssl: boolean;
-  extraArgs: string;
-}
-
-interface Transfer {
-  locked: boolean;
+	ssl: boolean;
+	extraArgs: string;
 }
 
 interface Props {
-  addTransfer(): any;
-  compileTransfers(): any;
-  clearTransfers(): any;
-  toggleSSL(): any;
-  addExtraArgs(args: string): any;
-  ssl?: boolean;
-  locked?: boolean;
+	addTransfer(): any;
+	compileTransfers(): any;
+	clearTransfers(): any;
+	toggleSSL(): any;
+	importTransfers(obj: TransferState): any;
+	ssl?: boolean;
+	locked?: boolean;
+	transfer: TransferState;
 }
 
 interface State {
-  compiler: Compiler;
-  transfer: Transfer;
+	compiler: Compiler;
+	transfer: TransferState;
 }
 
 function ActionBar({
-  addTransfer,
-  compileTransfers,
-  clearTransfers,
-  toggleSSL,
-  ssl,
-  locked,
+	addTransfer,
+	compileTransfers,
+	clearTransfers,
+	toggleSSL,
+	importTransfers,
+	ssl,
+	transfer,
 }: Props) {
-  return (
-    <React.Fragment>
-      <ButtonToolbar className="actionbar--mobile-fixed mt-3">
-        <ButtonGroup>
-          <Button
-            color="primary"
-            disabled={locked}
-            onClick={addTransfer}
-          >
-            Add Transfer
-          </Button>
-          <Button
-            color="warning"
-            disabled={locked}
-            onClick={() => {
-              clearTransfers();
-              compileTransfers();
-            }}
-          >
-            Clear
-          </Button>
-        </ButtonGroup>
-      </ButtonToolbar>
-      <FormGroup check className="mt-3">
-        <Label>
-          <Input
-            type="checkbox"
-            disabled={locked}
-            checked={ssl}
-            onChange={() => {
-              toggleSSL();
-              compileTransfers();
-            }}
-          />
-          Toggle SSL
-        </Label>
-      </FormGroup>
-    </React.Fragment>
-  );
+	const importFileInput = React.useRef(null);
+
+	function triggerImport() {
+		(importFileInput!.current! as any).click();
+	}
+
+	function handleImport(files: FileList) {
+		const file = files[0];
+
+		if (file) {
+			const reader = new FileReader();
+			reader.readAsText(file, 'UTF-8');
+			reader.onload = (evt: any) => {
+				if (isJSON(evt.target.result)) {
+					importTransfers(JSON.parse(evt.target.result));
+				}
+			};
+			reader.onerror = evt => {
+				console.log('error loading file');
+			};
+
+			(importFileInput!.current! as any).value = '';
+		}
+	}
+
+	return (
+		<React.Fragment>
+			<input
+				type="file"
+				ref={importFileInput}
+				onChange={(e: any) => handleImport(e.target.files)}
+				className="d-none"
+			/>
+			<Row>
+				<Col xs="auto">
+					<ButtonToolbar className="mt-3">
+						<ButtonGroup>
+							<Button color="primary" disabled={transfer.locked} onClick={addTransfer}>
+								Add Transfer
+							</Button>
+							<Button
+								color="warning"
+								disabled={transfer.locked}
+								onClick={() => {
+									clearTransfers();
+									compileTransfers();
+								}}
+							>
+								Clear
+							</Button>
+						</ButtonGroup>
+					</ButtonToolbar>
+				</Col>
+				<Col xs="auto">
+					<ButtonToolbar className="mt-3">
+						<ButtonGroup>
+							<Button
+								color="primary"
+								tag="a"
+								href={`data:application/octet-stream;charset=utf-16le;base64,${base64.encode(
+									JSON.stringify({
+										transfers: transfer.transfers,
+										inputs: transfer.inputs,
+										count: transfer.count,
+									})
+								)}`}
+								download={`imapsync_log-transfers-${new Date().toISOString()}.json`}
+							>
+								Export
+							</Button>
+							<Button color="warning" disabled={transfer.locked} onClick={() => triggerImport()}>
+								Import
+							</Button>
+						</ButtonGroup>
+					</ButtonToolbar>
+				</Col>
+			</Row>
+			<FormGroup check className="mt-3">
+				<Label>
+					<Input
+						type="checkbox"
+						disabled={transfer.locked}
+						checked={ssl}
+						onChange={() => {
+							toggleSSL();
+							compileTransfers();
+						}}
+					/>
+					Toggle SSL
+				</Label>
+			</FormGroup>
+		</React.Fragment>
+	);
 }
 
 const mapStateToProps = (state: State) => ({
-  ssl: state.compiler.ssl,
-  extraArgs: state.compiler.extraArgs,
-  locked: state.transfer.locked,
+	ssl: state.compiler.ssl,
+	extraArgs: state.compiler.extraArgs,
+	transfer: state.transfer,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) =>
-  bindActionCreators(
-    {
-      addTransfer,
-      compileTransfers,
-      clearTransfers,
-      toggleSSL,
-    },
-    dispatch,
-  );
+	bindActionCreators(
+		{
+			addTransfer,
+			compileTransfers,
+			clearTransfers,
+			toggleSSL,
+			importTransfers,
+		},
+		dispatch
+	);
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+	mapStateToProps,
+	mapDispatchToProps
 )(ActionBar);
