ad9133af35e278bdd5f1bc0f0de4e4bd8709a103
Casper Engelmann <cme@web-koncept.dk>
2019-04-02 12:44:31 +0200

    commands are now sent from client to server

--- src/components/Controller/index.tsx
+++ src/components/Controller/index.tsx
@@ -3,6 +3,7 @@ import { ButtonGroup, Button } from 'reactstrap';
 import { connect } from 'react-redux';
 import { Dispatch, bindActionCreators } from 'redux';
 
+import { setCancelled } from '../../actions/transfer';
 import { clearPids, clearLogs } from '../../actions/process';
 import { compileTransfers } from '../../actions/compiler';
 
@@ -16,6 +17,7 @@ interface Command {
 
 interface Transfer {
   locked: boolean;
+  cancelled: boolean;
 }
 
 interface Compiler {
@@ -27,35 +29,86 @@ interface State {
   compiler: Compiler;
 }
 
+interface Pid {
+  index: number;
+  pid: number;
+}
+
 interface Props {
   compileTransfers(): void;
   clearPids(): void;
   clearLogs(): void;
+  setCancelled(cancelled: boolean): void;
   locked: boolean;
+  cancelled: boolean;
   commandList: string[];
 }
 
-function Controller({ compileTransfers, clearPids, clearLogs, commandList, locked }: Props) {
+export type NewCommand = {
+  index: number;
+  command: Command;
+}
+
+function Controller({ 
+  compileTransfers, 
+  clearPids, 
+  clearLogs, 
+  setCancelled,
+  commandList, 
+  locked, 
+  cancelled, 
+}: Props) {
   async function execute() {
+    setCancelled(false);
 		await compileTransfers();
 
 		if (commandList.length) {
-			ipcRenderer.send('command', commandList);
+			ipcRenderer.send('command', {
+        command: commandList[0],
+        index: 0,
+      });
 		}
   }
 
-  function reset() {
+  function clear() {
 		clearPids();
 		clearLogs();
   }
   
+  function exitListener(event: any, pid: Pid) {
+    if (commandList.length >= pid.index + 1 && !cancelled) {
+      ipcRenderer.send('command', {
+        command: commandList[pid.index + 1],
+        index: pid.index + 1,
+      })
+    } else {
+      setCancelled(!cancelled);
+    }
+  }
+
+  React.useEffect(() => {
+    ipcRenderer.on('command-exit', exitListener);
+    
+    return () => {
+      ipcRenderer.removeListener('command-exit', exitListener);
+    }
+  });
+  
   return (
     <ButtonGroup>
-      <Button color="primary" onClick={execute} disabled={locked}>
+      <Button 
+        color="primary"
+        onClick={execute}
+        disabled={locked}
+      >
         Execute
       </Button>
-      <Button color="warning" onClick={reset} disabled={locked}>
-        Reset
+      <Button 
+        color="warning"
+        onClick={clear}
+        disabled={locked}
+      >
+        Clear
       </Button>
     </ButtonGroup>
   )
@@ -63,13 +116,15 @@ function Controller({ compileTransfers, clearPids, clearLogs, commandList, locke
 
 const mapStateToProps = (state: State) => ({
   locked: state.transfer.locked,
-	commandList: state.compiler.command.json,
+  commandList: state.compiler.command.json,
+  cancelled: state.transfer.cancelled,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => bindActionCreators({
   compileTransfers,
   clearPids,
   clearLogs,
+  setCancelled,
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Controller);
